7610fbede3eafc7034845ccca9c0cc3ff0216202
SoYoung Choi <101785763+ChoiSoYoung0114@users.noreply.github.com>
2022-05-19 15:14:59 +0900

    MODIFY_2

--- ossp.py
+++ ossp.py
@@ -1,6 +1,8 @@
 name = str(input("이름을 입력하세요: "))
 studentID = int(input("학번을 입력하세요: "))
+univ = str(input("대학교를 입력하세요: "))
 
 print("<출력>")
 print("이름: ",name)
 print("학번: ",studentID)
+print("대학교: ",univ)
